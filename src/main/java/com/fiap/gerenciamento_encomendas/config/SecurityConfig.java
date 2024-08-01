f22121efc7bf52a56f748427f4d059d38c2f0334
Lucianne Charro <luciannecharro@MacBook-Pro-de-Lucianne.local>
2024-07-31 22:18:43 -0300

    versao final do projeto

--- src/main/java/com/fiap/gerenciamento_encomendas/config/SecurityConfig.java
+++ src/main/java/com/fiap/gerenciamento_encomendas/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
